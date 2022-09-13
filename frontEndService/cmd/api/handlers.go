cf5fe40a73f8da10ffd2647515ab92d9646b2b40
VJ-Vijay77 <vijaydinesh77vj@gmail.com>
2022-09-13 18:15:30 +0530

    working

--- frontEndService/cmd/api/handlers.go
+++ frontEndService/cmd/api/handlers.go
@@ -1,9 +1,42 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/gin-gonic/gin"
+	"golang.org/x/text/cases"
 )
+
+type Message struct{
+	Name string `json:"name"`
+	Db string `json:"db"`
+}
 
 func(app *Config) HomePage(c *gin.Context) {
 	 c.JSON(200,"Hits on HomePage")
+}
+
+func(app *Config) Manage(c *gin.Context) {
+	var request Message
+
+	if err := c.ShouldBindJSON(&request); err != nil {
+		log.Println(err)
+		c.JSON(500,"Could not bind")
+		return
+	}
+	
+
+	switch request.Db {
+	case "postgres":
+		c.JSON(200,"ok")
+	case "mongo":
+		c.JSON(200,"Under working")
+	default:
+			c.JSON(404,"No such database")
+		}	
+}
+
+func(app *Config) ToPostgres(data Message) {
+
 }
