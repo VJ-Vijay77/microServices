d236b929a3005e45925f99e832bd8692edc2cca4
VJ-Vijay77 <vijaydinesh77vj@gmail.com>
2022-09-13 12:43:45 +0530

    mongo is going good

---
+++ mongoService/cmd/api/db.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+)
+
+func ConnectMongo() *Config {
+	client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Disconnect(ctx)
+
+	err = client.Ping(ctx, readpref.Primary())
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Connection to Mongo Successful...")
+	}
+
+	return &Config{
+		Db: client,
+	}
+
+}
+
+func(app *Config) GetMongoCollection() (*mongo.Collection) {
+	collection := app.Db.Database(os.Getenv("DB_NAME")).Collection(os.Getenv("COLLECTION_NAME"))
+	return collection
+}
--- mongoService/cmd/api/main.go
+++ mongoService/cmd/api/main.go
@@ -1,61 +1,50 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
-	"os"
-	"time"
 
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
-	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
 // var MONGO_URI = "mongodb://localhost:27017"
-//  var MONGO_URI = "mongodb://mongodb:27017"
+//
+//	var MONGO_URI = "mongodb://mongodb:27017"
 var PORT = ":8081"
 
-type Config struct{
-	
+type Config struct {
+	Db *mongo.Client
 }
 
 func main() {
-	app  := Config {}
-	client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGO_URI")))
-	if err != nil {
-		log.Fatal(err)
-	}
+	var app = &Config{}
+	// client, err := mongo.NewClient(options.Client().ApplyURI(os.Getenv("MONGO_URI")))
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 
-	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
-	defer cancel()
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Disconnect(ctx)
+	// ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
+	// defer cancel()
+	// err = client.Connect(ctx)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer client.Disconnect(ctx)
 
-	err = client.Ping(ctx, readpref.Primary())
-	if err != nil {
-		log.Fatal(err)
-	} else {
-		fmt.Println("Connection to Mongo Successful...")
-	}
-
-	database, err := client.ListDatabaseNames(ctx, bson.M{})
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	_ = database
+	// err = client.Ping(ctx, readpref.Primary())
+	// if err != nil {
+	// 	log.Fatal(err)
+	// } else {
+	// 	fmt.Println("Connection to Mongo Successful...")
+	// }
+	
+	app = ConnectMongo()
 
 	r := gin.Default()
 	app.Routess(r)
 	log.Println("Starting MongoDB Service on Port ", PORT)
-	
-	err = r.Run(PORT)
+
+	err := r.Run(PORT)
 	if err != nil {
 		log.Panic(err)
 	}
