f12fd6b2f3cb03c41afcaf8ab8c520b0cb5739a5
VJ-Vijay77 <vijaydinesh77vj@gmail.com>
2022-09-14 11:04:54 +0530

    writing helpers

--- frontEndService/cmd/api/helpers.go
+++ frontEndService/cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -15,7 +16,7 @@ type jsonResponse struct {
 
 
 
-func (app *Config) errorJSON(c gin.ResponseWriter, err error, status ...int) {
+func (app *Config) errorJSON(c gin.ResponseWriter, err error, status ...int) error {
 	statusCode := http.StatusBadRequest
 
 	if len(status) > 0 {
@@ -26,6 +27,28 @@ func (app *Config) errorJSON(c gin.ResponseWriter, err error, status ...int) {
 	jsRes.Error = true
 	jsRes.Message = err.Error()
 
-	return 
+	return app.writeJSON(c,statusCode,jsRes)
 
+}
+
+func(app *Config) writeJSON(c gin.ResponseWriter,status int,data any,headers ...http.Header) error {
+	out,err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	if len(headers) > 0 {
+		for key,value := range headers[0] {
+			c.Header()[key] = value
+		}
+	}
+
+	c.Header().Set("Content-Type","application/json")
+	c.WriteHeader(status)
+	_,err = c.Write(out)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
